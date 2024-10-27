eddd3d17d8bab2028b7f2f73a968d48d1a112456
Ok JaeOok <okjaeook98@gmail.com>
2024-10-27 19:43:32 +0900

    feat: initial controller 추가

---
+++ src/main/java/org/sesac/market/ProductController.java
@@ -0,0 +1,14 @@
+package org.sesac.market;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class ProductController {
+    @GetMapping
+    public String home() {
+        return "<h2>Product Server</h2>";
+    }
+}
