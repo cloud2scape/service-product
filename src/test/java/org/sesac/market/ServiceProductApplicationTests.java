54e240eaf5a46399b9604c1c63b2b552ee6de492
Ok JaeOok <okjaeook98@gmail.com>
2024-10-27 13:40:40 +0900

    init: 프로젝트 생성

---
+++ src/test/java/org/sesac/market/ServiceProductApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sesac.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
