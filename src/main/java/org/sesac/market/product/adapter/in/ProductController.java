ae917b2a71fe3d23c894248576f604ab2a4aa556
Ok JaeOok <okjaeook98@gmail.com>
2024-11-02 01:00:45 +0900

    chore: 패키지 구조 변경
    `+` market -> market.{domain}
    `+` hexagonal 아키텍처 패키지 구성
    
    BREAKING-CHANGE: 프로젝트 root 패키지 변경

--- src/main/java/org/sesac/market/product/adapter/in/ProductController.java
+++ src/main/java/org/sesac/market/product/adapter/in/ProductController.java
@@ -1,4 +1,4 @@
-package org.sesac.market;
+package org.sesac.market.product.adapter.in;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/test/java/org/sesac/market/product/ServiceProductApplicationTests.java
+++ src/test/java/org/sesac/market/product/ServiceProductApplicationTests.java
@@ -1,4 +1,4 @@
-package org.sesac.market;
+package org.sesac.market.product;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
