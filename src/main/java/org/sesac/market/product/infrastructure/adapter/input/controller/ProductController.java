040d6d9d55e43b6902a6ae5d29795b4e9d804746
Ok JaeOok <okjaeook98@gmail.com>
2024-11-03 18:52:15 +0900

    chore: 헥사고날 구성

--- src/main/java/org/sesac/market/product/infrastructure/adapter/input/controller/ProductController.java
+++ src/main/java/org/sesac/market/product/infrastructure/adapter/input/controller/ProductController.java
@@ -1,4 +1,4 @@
-package org.sesac.market.product.adapter.in;
+package org.sesac.market.product.infrastructure.adapter.input.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
