7a141556a0d825448cb5135b86fca8de92eaea68
laigasus <okjaeook98@gmail.com>
2024-11-21 20:31:55 +0900

    feat: product 상품 command usecase 추가
    `+` 재고 변경

--- src/main/java/org/sesac/market/product/application/port/input/ProductCommand.java
+++ src/main/java/org/sesac/market/product/application/port/input/ProductCommand.java
@@ -3,6 +3,7 @@ package org.sesac.market.product.application.port.input;
 import org.sesac.market.product.application.dto.request.CreateProductRequest;
 import org.sesac.market.product.application.dto.request.DeleteProductRequest;
 import org.sesac.market.product.application.dto.request.UpdateProductRequest;
+import org.sesac.market.product.application.dto.request.UpdateProductStockDecreaseRequest;
 import org.sesac.market.product.domain.model.Product;
 
 public interface ProductCommand {
@@ -11,4 +12,6 @@ public interface ProductCommand {
     Product update(UpdateProductRequest request);
 
     boolean delete(DeleteProductRequest request);
+
+    Product updateStock(UpdateProductStockDecreaseRequest request);
 }
