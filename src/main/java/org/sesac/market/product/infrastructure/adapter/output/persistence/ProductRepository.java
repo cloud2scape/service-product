2657b4874a01b71d9d111309b97622574736dcaf
laigasus <okjaeook98@gmail.com>
2024-12-07 14:11:44 +0900

    chore: 파일 이동 및 정리

--- src/main/java/org/sesac/market/product/infrastructure/adapter/output/persistence/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package org.sesac.market.product.infrastructure.adapter.output.persistence;
-
-import org.sesac.market.product.domain.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
